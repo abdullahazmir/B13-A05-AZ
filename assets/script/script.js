f2c3e4ad76225a6b5af2c20901a8561a8f972573
abdullahazmir <ab.azmir@gmail.com>
2026-03-08 00:00:15 +0600

    js functionality starts

---
+++ assets/script/script.js
@@ -0,0 +1,11 @@
+const userName = document.getElementById('user-name')
+const password = document.getElementById('password')
+
+function btnSignIn(){
+    userName1= userName.value;
+    password1= password.value;
+
+    if(userName1==admin && password1==admin123){
+        
+    }
+}
