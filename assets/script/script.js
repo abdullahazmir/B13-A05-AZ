207a431fd7b5496f5597987c9e523ce9619a3364
abdullahazmir <ab.azmir@gmail.com>
2026-03-08 23:13:55 +0600

    search functionality added

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,201 @@
+const createElements = (arr)=>{
+    const htmlElements= arr.map(el=>`<div class=" badge bg-orange-300 badge-info">${el}</div>`) ;
+
+    return htmlElements.join(" ")
+};
+
+
+
+
+
+
+// const createElements = (arr) => {
+//   const htmlElements = arr.map((el) => `<badge class="btn bg-orange-300 rounded-full badge-info">${el}</badge>`);
+//   return htmlElements.join(" ");
+// };
+
+
 const userName = document.getElementById('user-name')
 const password = document.getElementById('password')
+const loginSection = document.getElementById('login-section')
+const mainSection = document.getElementById('main-section')
+const issueContainer =document.getElementById('issue-container')
 
-function btnSignIn(){
-    userName1= userName.value;
-    password1= password.value;
 
-    if(userName1==admin && password1==admin123){
-        
+
+const btnSignIn =()=>{
+    const userName1= userName.value;
+   const password1= password.value;
+//    console.log(userName1)
+//    console.log(password1)
+
+    if(userName1=='admin' && password1=='admin123'){
+        loginSection.classList.add('hidden');
+        mainSection.classList.remove('hidden')
+
+    } else{
+        alert('wrong username or password');
+        return;
     }
+};
+
+// toggle effect
+
+
+
+// fetching all issues
+// {
+//     "id": 1,
+//     "title": "Fix navigation menu on mobile devices",
+//     "description": "The navigation menu doesn't collapse properly on mobile devices. Need to fix the responsive behavior.",
+//     "status": "open",
+//     "labels": [
+//         "bug",
+//         "help wanted"
+//     ],
+//     "priority": "high",
+//     "author": "john_doe",
+//     "assignee": "jane_smith",
+//     "createdAt": "2024-01-15T10:30:00Z",
+//     "updatedAt": "2024-01-15T10:30:00Z"
+// }
+
+const loadAllIssues = ()=>{
+    const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues'
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data=> {
+        // console.log(data)
+        displayAllIssues(data.data)
+    })
+    
+    
 }
+
+const displayAllIssues =(array)=>{
+    issueContainer.innerHTML ='';
+    console.log(array)
+    array.forEach((issue) =>{
+        const bdDate = new Date(issue.createdAt).toLocaleString("en-BD", {
+            timeZone: "Asia/Dhaka"
+        });
+        const iss = document.createElement('div');
+        // console.log(issue)
+        iss.innerHTML =`
+          <div onclick="loadSingleIssue(${issue.id})" class=" h-full space-y-4 shadow-md p-5 rounded-md">
+                    <div class="flex justify-between">
+                        <div><img src="assets/Open-Status.png" alt=""></div>
+                        <div class="badge badge-warning">${issue.priority}</div>
+                    </div>
+                    <h2 class="text-xl font-medium">${issue.title}</h2>
+                    <p>${issue.description}</p>
+                    <div  class="flex justify-start  gap-4">
+                      ${createElements(issue.labels)}
+                    </div>
+                    <hr>
+                    <div>
+                        <p>${issue.author}</p>
+                        <p>${bdDate}</p>
+                    </div>
+
+
+                </div>
+        `
+
+        issueContainer.appendChild(iss)
+    })
+
+    }
+    loadAllIssues();
+
+    // modal load and display
+
+    const loadSingleIssue = async (id)=>{
+        const url =`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+        const res =await fetch(url)
+        const details = await res.json()
+        displaySingleIssue(details.data)
+
+    }
+    const displaySingleIssue = (issue)=>{
+        
+         const bdDate = new Date(issue.createdAt).toLocaleString("en-BD", {
+            timeZone: "Asia/Dhaka"
+        })
+
+        const modalContainer = document.getElementById('modal-container');
+        modalContainer.innerHTML=`
+          <h2 class="text-xl font-medium">${issue.title}</h2>
+                        <div class="space-y-4">
+                        <div class="flex gap-4 text-center">
+                            <div class="badge badge-warning"> ${issue.status}ed by </div>
+                            <ul class="flex gap-5">
+                                <li> ${issue.author}</li>
+                                <li>${bdDate}</li>
+                            </ul>
+                            </div>
+
+                            <div class="flex gap-5">
+                             ${createElements(issue.labels)}
+                              
+                            </div>
+
+                            <p>${issue.description}</p>
+
+                            <div class="flex justify-around bg-gray-300 rounded-md space-y-4">
+                                <div>
+                                    <p>Assignee:</p>
+                                    <h2>${issue.assignee}</h2>
+                                </div>
+                                <div>
+                                    <p>Priority:</p>
+                                    <div class="badge badge-warning">${issue.priority}</div>
+
+                                </div>
+                            </div>
+                        </div>
+        
+        `
+
+        document.getElementById('my_modal').showModal();
+
+    }
+
+    // search issue
+
+    // const loadSearch = async()=>{
+    //     const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q={searchText}`
+    //     const res = await fetch(url);
+    //     const search = await res.json();
+    //     displaySearch(search.data)
+    // }
+    //     const displaySearch= (data)=>{
+    //     const searchInput = document.getElementById('input');
+    //     const searchBtn = document.getElementById('search')
+
+    //     }
+
+    // loadSearch()
+
+
+    const displaySearch = async () => {
+
+    const searchText = document.getElementById('input-search').value;
+
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchText}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+   
+
+    displayAllIssues(data.data);
+}
+
+
+  
+
+
+    
+
