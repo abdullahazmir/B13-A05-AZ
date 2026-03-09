621c847253a96f9f7521a1bfbe63ecc14e278d61
abdullahazmir <ab.azmir@gmail.com>
2026-03-09 06:45:02 +0600

    color changing done

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,5 @@
-const createElements = (arr)=>{
-    const htmlElements= arr.map(el=>`<div class=" badge bg-orange-300 badge-info">${el}</div>`) ;
+const createElements = (arr) => {
+    const htmlElements = arr.map(el => `<div class=" badge bg-orange-300 badge-info">${el}</div>`);
 
     return htmlElements.join(" ")
 };
@@ -19,95 +19,119 @@ const userName = document.getElementById('user-name')
 const password = document.getElementById('password')
 const loginSection = document.getElementById('login-section')
 const mainSection = document.getElementById('main-section')
-const issueContainer =document.getElementById('issue-container')
+const issueContainer = document.getElementById('issue-container')
+let issueNumber = document.getElementById('issue-number')
 
 
 
-const btnSignIn =()=>{
-    const userName1= userName.value;
-   const password1= password.value;
-//    console.log(userName1)
-//    console.log(password1)
+const btnSignIn = () => {
+    const userName1 = userName.value;
+    const password1 = password.value;
+    //    console.log(userName1)
+    //    console.log(password1)
 
-    if(userName1=='admin' && password1=='admin123'){
+    if (userName1 == 'admin' && password1 == 'admin123') {
         loginSection.classList.add('hidden');
         mainSection.classList.remove('hidden')
 
-    } else{
+    } else {
         alert('wrong username or password');
         return;
     }
 };
 
 // toggle effect
-
-
-
-function setActiveButton(clickedBtn){
+function setActiveButton(clickedBtn) {
 
     const buttons = document.querySelectorAll('.filter-btn');
     // console.log(buttons)
 
-    buttons.forEach(btn=>{
+    buttons.forEach(btn => {
         btn.classList.remove('btn-primary'); // active color remove
     });
 
     clickedBtn.classList.add('btn-primary'); // active color add
 }
 
-function filterIssues(status){
-     const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues'
-     const clickedBtn = event.target;
+function filterIssues(status) {
+    const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues'
+    const clickedBtn = event.target;
     setActiveButton(clickedBtn);
 
     fetch(url)
-    .then(res => res.json())
-    .then(data=> {
-        // console.log(data)
-        const allIssues =data.data
-        
-    let filteredIssues;
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            const allIssues = data.data
 
-    if(status === 'all'){
-        filteredIssues = allIssues;
-    } else{
-        filteredIssues = allIssues.filter(issue => issue.status === status);
-    }
+            let filteredIssues;
 
-    issueContainer.innerHTML = '';
-    displayAllIssues(filteredIssues);
-    })
+            if (status === 'all') {
+                filteredIssues = allIssues;
+            } else {
+                filteredIssues = allIssues.filter(issue => issue.status === status);
+            }
 
-    
+            issueContainer.innerHTML = '';
+            displayAllIssues(filteredIssues);
+        })
+
+
+
+}
+
+// loader issues
+
+const showLoader = () => {
+    document.getElementById('loader').classList.remove('hidden')
+
+}
+const hideLoader = () => {
+    document.getElementById('loader').classList.add('hidden')
 
 }
 
 
 
-const loadAllIssues = ()=>{
+
+const loadAllIssues = () => {
+    showLoader();
+
     const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues'
 
     fetch(url)
-    .then(res => res.json())
-    .then(data=> {
-        // console.log(data)
-        displayAllIssues(data.data)
-    })
-    
-    
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            hideLoader();
+            displayAllIssues(data.data)
+        })
+
+
+
 }
 
-const displayAllIssues =(array)=>{
-    issueContainer.innerHTML ='';
+const displayAllIssues = (array) => {
+    issueNumber=  issueContainer.length
+
+    issueContainer.innerHTML = '';
     console.log(array)
-    array.forEach((issue) =>{
+    array.forEach((issue) => {
         const bdDate = new Date(issue.createdAt).toLocaleString("en-BD", {
             timeZone: "Asia/Dhaka"
         });
         const iss = document.createElement('div');
+
+
+        const borderColor = issue.status === "open" 
+            ? "border-green-500" 
+            : "border-purple-500";
+        
+       
+
         // console.log(issue)
-        iss.innerHTML =`
-          <div onclick="loadSingleIssue(${issue.id})" class=" h-full space-y-4 shadow-md p-5 rounded-md">
+        iss.innerHTML = `
+        <div id="card" onclick="loadSingleIssue(${issue.id})" class=" h-full space-y-4 shadow-md border-t-4  ${borderColor} p-5 rounded-md">
                     <div class="flex justify-between">
                         <div><img src="assets/Open-Status.png" alt=""></div>
                         <div class="badge badge-warning">${issue.priority}</div>
@@ -130,26 +154,26 @@ const displayAllIssues =(array)=>{
         issueContainer.appendChild(iss)
     })
 
-    }
-    loadAllIssues();
+}
+loadAllIssues();
 
-    // modal load and display
+// modal load and display
 
-    const loadSingleIssue = async (id)=>{
-        const url =`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
-        const res =await fetch(url)
-        const details = await res.json()
-        displaySingleIssue(details.data)
+const loadSingleIssue = async (id) => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`
+    const res = await fetch(url)
+    const details = await res.json()
+    displaySingleIssue(details.data)
 
-    }
-    const displaySingleIssue = (issue)=>{
-        
-         const bdDate = new Date(issue.createdAt).toLocaleString("en-BD", {
-            timeZone: "Asia/Dhaka"
-        })
+}
+const displaySingleIssue = (issue) => {
 
-        const modalContainer = document.getElementById('modal-container');
-        modalContainer.innerHTML=`
+    const bdDate = new Date(issue.createdAt).toLocaleString("en-BD", {
+        timeZone: "Asia/Dhaka"
+    })
+
+    const modalContainer = document.getElementById('modal-container');
+    modalContainer.innerHTML = `
           <h2 class="text-xl font-medium">${issue.title}</h2>
                         <div class="space-y-4">
                         <div class="flex gap-4 text-center">
@@ -182,28 +206,28 @@ const displayAllIssues =(array)=>{
         
         `
 
-        document.getElementById('my_modal').showModal();
+    document.getElementById('my_modal').showModal();
 
-    }
+}
 
-    // search issue
+// search issue
 
-    // const loadSearch = async()=>{
-    //     const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q={searchText}`
-    //     const res = await fetch(url);
-    //     const search = await res.json();
-    //     displaySearch(search.data)
-    // }
-    //     const displaySearch= (data)=>{
-    //     const searchInput = document.getElementById('input');
-    //     const searchBtn = document.getElementById('search')
+// const loadSearch = async()=>{
+//     const url = `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q={searchText}`
+//     const res = await fetch(url);
+//     const search = await res.json();
+//     displaySearch(search.data)
+// }
+//     const displaySearch= (data)=>{
+//     const searchInput = document.getElementById('input');
+//     const searchBtn = document.getElementById('search')
 
-    //     }
+//     }
 
-    // loadSearch()
+// loadSearch()
 
 
-    const displaySearch = async () => {
+const displaySearch = async () => {
 
     const searchText = document.getElementById('input-search').value;
 
@@ -212,14 +236,14 @@ const displayAllIssues =(array)=>{
     const res = await fetch(url);
     const data = await res.json();
 
-   
+
 
     displayAllIssues(data.data);
 }
 
 
-  
 
 
-    
+
+
 
